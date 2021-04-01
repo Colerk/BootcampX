5b695b93a78845c1ac2f76b5774bbbd18e3739d6
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-01 16:20:10 +0000

    adds paramaterized functions

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,14 +9,14 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = $1
+ORDER BY teachers.name;
+`, [`${process.argv[2] || 'JUL02'}`])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
